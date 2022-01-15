c4405f7460d3a3d18f7cc2313f0949e7e2b48f6c
milanzigmond <milanzigmond@milanzigmond.com>
2022-01-15 09:43:13 +0100

    sliders

--- sketch.js
+++ sketch.js
@@ -1,25 +1,40 @@
+let sliderSW
+let sliderRepeat
+let sliderSWValue
+let sliderRepeatValue
+
 function setup() {
 	createCanvas(innerWidth, innerHeight)
 	angleMode(DEGREES)
 	rectMode(CENTER)
 	colorMode(HSB, 255, 100, 100)
 	// noLoop()
+
+	// sliders
+	createSliders()
 }
 
 function draw() {
 	background(0)
 	noFill()
 
+	let sw = sliderSW.value()
+	let repeat = sliderRepeat.value()
+	sliderSWValue.html(sw)
+	sliderRepeatValue.html(repeat)
+
+	let angle = 360 / repeat
+
 	translate(width / 2, height / 2)
 
-	for (let i = 0; i < 6; i++) {
-		strokeWeight(10)
+	for (let i = 0; i < repeat; i++) {
+		strokeWeight(sw)
 		drawSquares()
-		rotate(45)
+		rotate(angle)
 	}
 }
 
-function mouseClicked() {
+function doubleClicked() {
 	if (isLooping()) noLoop()
 	else loop()
 }
@@ -42,13 +57,33 @@ function drawSquares() {
 			rotate(sin(frameCount / 2 + i * 2) * 20)
 			rect(0, 0, i * 3, i * 3, i / 2)
 		}
-		// beginShape()
-		// vertex(0, 0)
-		// vertex(i * 4, 0)
-		// vertex(i * 4, i * 4)
-		// vertex(0, i * 4)
-		// endShape(CLOSE)
 
 		pop()
 	}
+}
+
+function createSliders() {
+	let pSW = createP("STROKE WEIGHT")
+	pSW.style("font-size", "12px")
+	pSW.style("color", "white")
+	pSW.position(10, 0)
+	sliderSW = createSlider(1, 100, 4, 1)
+	sliderSW.position(10, 30)
+	sliderSW.style("width", "100px")
+	sliderSWValue = createElement("h1")
+	sliderSWValue.style("font-size", "16px")
+	sliderSWValue.style("color", "white")
+	sliderSWValue.position(120, 20)
+
+	let pR = createP("REPEAT")
+	pR.style("font-size", "12px")
+	pR.style("color", "white")
+	pR.position(10, 50)
+	sliderRepeat = createSlider(1, 12, 6, 1)
+	sliderRepeat.position(10, 80)
+	sliderRepeat.style("width", "100px")
+	sliderRepeatValue = createElement("h1")
+	sliderRepeatValue.style("font-size", "16px")
+	sliderRepeatValue.style("color", "white")
+	sliderRepeatValue.position(120, 70)
 }
