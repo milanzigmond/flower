c10cbd52a6a12c9ac99958b5c14a53bac668e255
milanzigmond <milanzigmond@milanzigmond.com>
2022-01-16 10:23:21 +0100

    sliders, window resize

--- sketch.js
+++ sketch.js
@@ -6,13 +6,17 @@ let sliderDistance
 let sliderDistanceValue
 let sliderRotation
 let sliderRotationValue
+let sliderSpeed
+let sliderSpeedValue
+let sliderSize
+let sliderSizeValue
 
 function setup() {
 	createCanvas(innerWidth, innerHeight)
 	angleMode(DEGREES)
 	rectMode(CENTER)
 	colorMode(HSB, 255, 100, 100)
-	// noLoop()
+	noLoop()
 
 	// sliders
 	createSliders()
@@ -29,6 +33,8 @@ function draw() {
 	sliderRepeatValue.html(repeat)
 	sliderDistanceValue.html(sliderDistance.value())
 	sliderRotationValue.html(sliderRotation.value())
+	sliderSpeedValue.html(sliderSpeed.value())
+	sliderSizeValue.html(sliderSize.value())
 
 	let angle = 360 / repeat
 
@@ -55,66 +61,74 @@ function drawDistance() {
 		let b = floor(map(sin(i * frameCount * 0.002), -1, 1, 10, 100))
 		let a = map(i, 0, 255, 0.4, 0)
 
-		// if (i % 2 > 0) {
-		// 	stroke("hsba(" + h + ", " + s + "%, " + b + "%, " + a + ")")
-		// 	rotate(cos(frameCount / 2 + i) * sliderRotation.value())
-		// 	rect(0, 0, i * 3, i * 3, i / 2)
-		// } else {
-		stroke("hsba(" + h + ", " + b + "%, " + s + "%, " + a + ")")
-		rotate(sin(frameCount / 2 + i) * sliderRotation.value())
-		rect(0, 0, i * 3, i * 3, i / 2)
-		// }
+		if (i % 2 > 0) {
+			stroke("hsba(" + h + ", " + s + "%, " + b + "%, " + a + ")")
+			rotate(
+				cos(frameCount / (10 - sliderSpeed.value()) + i) *
+					sliderRotation.value()
+			)
+			rect(0, 0, i * 3, i * 3, i / 2)
+		} else {
+			stroke("hsba(" + h + ", " + s + "%, " + b + "%, " + a + ")")
+			rotate(
+				sin(frameCount / (21 - sliderSpeed.value()) + i) *
+					sliderRotation.value()
+			)
+			rect(0, 0, i * sliderSize.value(), i * sliderSize.value(), i / 2)
+		}
 
 		pop()
 	}
 }
 
+function addLabel(label, x, y) {
+	let p = createP(label)
+	p.style("font-size", "13px")
+	p.style("color", "white")
+	p.position(x, y)
+}
+
+function addSlider(min, max, value, step, x, y) {
+	let s = createSlider(min, max, value, step)
+	s.position(x, y)
+	s.style("width", "200px")
+	return s
+}
+
+function addSliderValue(x, y) {
+	const sv = createElement("h1")
+	sv.style("font-size", "16px")
+	sv.style("color", "white")
+	sv.position(x, y)
+	return sv
+}
+
 function createSliders() {
-	let pSW = createP("STROKE WEIGHT")
-	pSW.style("font-size", "12px")
-	pSW.style("color", "white")
-	pSW.position(10, 0)
-	sliderSW = createSlider(1, 100, 4, 1)
-	sliderSW.position(10, 30)
-	sliderSW.style("width", "100px")
-	sliderSWValue = createElement("h1")
-	sliderSWValue.style("font-size", "16px")
-	sliderSWValue.style("color", "white")
-	sliderSWValue.position(120, 20)
+	addLabel("STROKE W", 10, 0)
+	sliderSW = addSlider(1, 100, 50, 1, 80, 10)
+	sliderSWValue = addSliderValue(290, 0)
 
-	let pRe = createP("REPEAT")
-	pRe.style("font-size", "12px")
-	pRe.style("color", "white")
-	pRe.position(10, 50)
-	sliderRepeat = createSlider(1, 12, 6, 1)
-	sliderRepeat.position(10, 80)
-	sliderRepeat.style("width", "100px")
-	sliderRepeatValue = createElement("h1")
-	sliderRepeatValue.style("font-size", "16px")
-	sliderRepeatValue.style("color", "white")
-	sliderRepeatValue.position(120, 70)
+	addLabel("REPEAT", 10, 30)
+	sliderRepeat = addSlider(1, 12, 6, 1, 80, 40)
+	sliderRepeatValue = addSliderValue(290, 30)
 
-	let pD = createP("DISTANCE")
-	pD.style("font-size", "12px")
-	pD.style("color", "white")
-	pD.position(10, 100)
-	sliderDistance = createSlider(1, 100, 20, 1)
-	sliderDistance.position(10, 130)
-	sliderDistance.style("width", "100px")
-	sliderDistanceValue = createElement("h1")
-	sliderDistanceValue.style("font-size", "16px")
-	sliderDistanceValue.style("color", "white")
-	sliderDistanceValue.position(120, 120)
+	addLabel("DISTANCE", 10, 60)
+	sliderDistance = addSlider(1, 100, 50, 1, 80, 70)
+	sliderDistanceValue = addSliderValue(290, 60)
 
-	let pRo = createP("ROTATION")
-	pRo.style("font-size", "12px")
-	pRo.style("color", "white")
-	pRo.position(10, 150)
-	sliderRotation = createSlider(1, 180, 50, 1)
-	sliderRotation.position(10, 180)
-	sliderRotation.style("width", "100px")
-	sliderRotationValue = createElement("h1")
-	sliderRotationValue.style("font-size", "16px")
-	sliderRotationValue.style("color", "white")
-	sliderRotationValue.position(120, 170)
+	addLabel("ROTATION", 10, 90)
+	sliderRotation = addSlider(1, 180, 90, 1, 80, 100)
+	sliderRotationValue = addSliderValue(290, 90)
+
+	addLabel("SPEED", 10, 120)
+	sliderSpeed = addSlider(1, 20, 10, 1, 80, 130)
+	sliderSpeedValue = addSliderValue(290, 120)
+
+	addLabel("SIZE", 10, 150)
+	sliderSize = addSlider(1, 6, 3, 1, 80, 160)
+	sliderSizeValue = addSliderValue(290, 150)
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
 }
